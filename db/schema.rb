3eda7e6e09b009e6e65c8618ce3abce44947c008
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-06-18 14:57:01 +0300

    added auth API using jwt

---
+++ db/schema.rb
@@ -0,0 +1,17 @@
+
+ActiveRecord::Schema.define(version: 2021_06_18_115212) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active"
+  end
+
+end
