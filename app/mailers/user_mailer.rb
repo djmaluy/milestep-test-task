5a646705e3df08370ebca485526b70812c8bcb48
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-05 19:15:44 +0300

    fixed deploy mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,6 @@ class UserMailer < ApplicationMailer
 
   def registration_confirmation(user, token)
     @user = user
-    p ENV["CLIENT_URL"]
-    p "============="
   	@url  = ENV["CLIENT_URL"] + '/confirmation_email/' + token.to_s
     mail(to: @user.email, subject: 'Registration to My App')
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,12 +37,12 @@ Rails.application.configure do
  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
+    address:               ENV['SMTP_SERVER'],
+    port:                  ENV['PORT'],
+    domain:                ENV['DOMAIN'],
     user_name:             ENV['GMAIL_LOGIN'],
     password:              ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
