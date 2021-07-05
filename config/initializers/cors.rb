028e58b4ce451ce40e2f92c711e3549887c58892
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-05 08:57:26 +0300

    fixed CORS for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,15 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do  
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
 	allow do
-	    origins 'milestep-test-frontend.herokuapp.com'
+	    origins 'https://milestep-frontend.herokuapp.com/'
 
 	    resource '*',
 	      headers: :any,
 	      methods: [:get, :post, :put, :patch, :delete, :options, :head],
 	      credentials: true
-	      
+
   end
 end
 # Rails.application.config.hosts << 'localhost:3001'
