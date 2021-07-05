73174095cba126a1942dde6bc0afc775c7c878b5
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-05 18:23:39 +0300

    fixed mailer url for deploy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
 
   def registration_confirmation(user, token)
     @user = user
+    p ENV["client_url"]
+    p "============="
   	@url  = ENV["client_url"] + '/confirmation_email/' + token.to_s
     mail(to: @user.email, subject: 'Registration to My App')
   end
