8802b3664f8d9747e281c147ec4e10574e94ea4b
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-06 14:52:37 +0300

    fixed current user

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,4 +16,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   end
 end
-# Rails.application.config.hosts << 'https://milestep-frontend.herokuapp.com'
+Rails.application.config.hosts << 'https://milestep-frontend.herokuapp.com'
