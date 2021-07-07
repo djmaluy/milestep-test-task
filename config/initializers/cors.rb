40bce14b4219f25fec278f3f2e9d43af566e6133
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-07 08:22:15 +0300

    fixed email

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 	allow do
-	    origins 'https://milestep-frontend.herokuapp.com', "http://localhost:3001"
+	    origins 'https://milestep-frontend.herokuapp.com'
 
 	    resource '*',
 	      headers: :any,
