283d9017893a53e546b09d1948b35199d4a9ee9c
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-07 08:17:03 +0300

    fixed mailer. not send emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,19 +35,19 @@ Rails.application.configure do
  
 
  
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:               ENV['SMTP_SERVER'],
-  #   port:                  ENV['PORT'],
-  #   domain:                ENV['DOMAIN'],
-  #   user_name:             ENV['GMAIL_LOGIN'],
-  #   password:              ENV['GMAIL_PASSWORD'],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  
-  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               ENV['SMTP_SERVER'],
+    port:                  ENV['PORT'],
+    domain:                ENV['DOMAIN'],
+    user_name:             ENV['GMAIL_LOGIN'],
+    password:              ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   
   # Print deprecation notices to the Rails logger.
