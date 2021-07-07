9dabda02fe4146787bb6a675644f8606a3235d2f
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-07 16:32:02 +0300

    fixed creating session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,4 @@
-# Rails.application.config.session_store :cookie_store, key: '_habit-helper', 
+Rails.application.config.session_store :cookie_store, key: '_your_app_session'
   #  domain: "milestep-frontend.herokuapp.com/"
   # same_site: ENV["SESSION_COOKIE_SAMESITE"], 
   # secure: ENV['SESSION_COOKIE_SECURE']
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_habit-helper', domain: :all
-    Rails.application.config.session_store :cookie_store, key: '_habit-helper' 
-end
