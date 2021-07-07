8efb9bf90e2d60c1cca8fa7ec881e26e0ae17f32
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-07 09:23:34 +0300

    fixed creating session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: ENV["SECRET_KEY_BASE"], same_site: :none, secure: true
+Rails.application.config.session_store :cookie_store, 
+  key: ENV["SECRET_KEY_BASE"], 
+  same_site: ENV["SESSION_COOKIE_SAMESITE"], 
+  secure: ENV['SESSION_COOKIE_SECURE']
+
+# app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+# app.config['SESSION_COOKIE_SECURE'] = True
