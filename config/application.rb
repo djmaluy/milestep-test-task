07a92ad5e4811adeab44821ebe626d6d4688f7e2
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-07 16:28:45 +0300

    fixed creating session

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module MilestepTestTask
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.middleware.use ActionDispatch::Cookies
-    # config.api_only = true
+   
     # config.middleware.use ActionDispatch::Cookies
     # config.middleware.use ActionDispatch::Session::CookieStore,
     #          key: '_cookie_name', path: '/', same_site: :none, secure: true
