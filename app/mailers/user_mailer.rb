3347fdcc318020297fdb4fedd84ac76308ff3571
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-09 09:40:54 +0300

    redirecting user fixed client_url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def registration_confirmation(user, token)
     @user = user
-  	@url  = "http://localhost:3001" + '/confirmation_email/' + token.to_s
+  	@url  = ENV["CLIENT_URL"] + '/confirmation_email/' + token.to_s
     mail(to: @user.email, subject: 'Registration to My App')
   end
 
