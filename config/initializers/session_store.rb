85cd1cfa4f74cda02d4583c417efd6b1953cdc1d
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-12 10:43:21 +0300

    fixed session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
-Rails.application.config.session_store :cookie_store, key: '_milestep_frontend_session', same_site: ENV["SESSION_COOKIE_SAMESITE"], 
-secure: ENV['SESSION_COOKIE_SECURE'], domain: ENV["CLIENT_DOMAIN"], tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_milestep_frontend_session',  domain: :all, tld_length: 2
   
   
