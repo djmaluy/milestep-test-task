69f9dc0dc761cad7095eb0c380f0f76245cc907d
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-12 10:32:57 +0300

    fixed creating session on prod

--- app/controllers/concerns/with_session.rb
+++ app/controllers/concerns/with_session.rb
@@ -17,7 +17,8 @@ module WithSession
       @user = user
       token = Jwt::EncryptionService.new(user_id: @user.id).token
       time = 24.hours.from_now
-      cookies.signed[:session] = { value: token, expires: time, secure: true, same_site: "None", domain: :all }
+      cookies.signed[:session] = { value: token, expires: time, httponly: Rails.env.production?, 
+                                    secure: true, same_site: "None", domain: ENV["CLIENT_DOMAIN"] }
     end
 
     def current_user
