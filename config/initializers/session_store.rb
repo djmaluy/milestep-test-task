ae8c69d94a16827322e02c613d9a7866579ac651
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-12 14:11:49 +0300

    fixed session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,11 @@
-Rails.application.config.session_store :cookie_store, key: '_milestep_frontend_session', same_site: ENV["SESSION_COOKIE_SAMESITE"], 
-secure: ENV['SESSION_COOKIE_SECURE']
+# Rails.application.config.session_store :cookie_store, key: '_milestep_frontend_session', same_site: ENV["SESSION_COOKIE_SAMESITE"], 
+# secure: ENV['SESSION_COOKIE_SECURE']
   
   
+Rails.application.config.session_store :cookie_store, {
+  :key => '_milestep_frontend_session',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 2
+}
