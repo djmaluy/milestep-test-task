879c73a1388ce271855a78e7c8cb4df67e4da130
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-13 10:46:41 +0300

    fixed cookies

--- app/controllers/concerns/with_session.rb
+++ app/controllers/concerns/with_session.rb
@@ -18,7 +18,7 @@ module WithSession
       token = Jwt::EncryptionService.new(user_id: @user.id).token
       time = 24.hours.from_now
       cookies.signed[:session] = { value: token, expires: time, httponly: true, 
-                                   same_site: :none, secure: true, domain: :all }
+                                   same_site: :none, secure: true, domain: "https://milestep-test-task-backend.herokuapp.com" }
     end
 
     def current_user
