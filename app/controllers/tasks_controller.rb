9ec0fc907c3182128aafc3fa7e4a6b144faea453
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-29 18:41:25 +0300

    fix deploy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :find_task, only: [:show, :update]
-  # before_action :authenticate!, only: [:index]
+  before_action :authenticate!, except: [:index]
 
   # GET all Tasks
   def index
