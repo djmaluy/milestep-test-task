91d67b6a4e0bd511efe1edb5fc25966492b38175
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-08-03 16:36:58 +0300

    added uploading pdf files

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,12 @@ class Task < ApplicationRecord
   validates :priority, numericality: true, presence: true
   validates :title, :description, length: { in: 2..100 }, presence: true
 
-  has_one_attached :image
-  
+  has_one_attached :some_file
+
+  def some_file_url
+    if some_file.attached?
+      S3_BUCKET.object(some_file.key).public_url
+    end
+  end
+
 end
